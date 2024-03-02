a15ea5decdce72d1bcd4579dabd68b3864e28b62
1salo <william-salo@hotmail.com>
2024-03-02 18:42:42 +0100

    added auth and other changes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,31 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="mr-5">
         SENDIGO
       </Link>
       <Link href="/safunkardet">SÅ FUNKAR DET</Link>
       <Link href="/priceplan">PRISPLANER</Link>
       <Link href="/help">HJÄLP</Link>
+      {status === "authenticated" && (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Logga ut
+          </Link>
+        </div>
+      )}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">LOGGA IN</Link>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
     </main>
   );
 }
