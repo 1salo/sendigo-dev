26b0dfec3dcccdbb1b0a426db1941c43aeb9525b
1salo <william-salo@hotmail.com>
2024-04-30 03:35:39 +0200

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt"; // Assuming you're using next-auth for handling JWT.
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  // Extract the token from the request cookies
   const token = await getToken({ req: request });
 
-  // Current path
   const path = new URL(request.url).pathname;
 
   // Routes that only non-authenticated users should access
   const publicRoutes = ["/sign-in", "/sign-up"];
 
-  // If the token exists and the user is trying to access a public route, redirect to dashboard
   if (token && publicRoutes.includes(path)) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  // List of protected routes
   const protectedRoutes = [
     "/dashboard",
     "/orders",
@@ -24,6 +20,7 @@ export async function middleware(request: NextRequest) {
     "/profile",
     "/settings",
     "/newshipment/compare",
+    "/contacts",
   ];
 
   // Check if the current route is protected, not an API route, and no token is found
@@ -32,11 +29,8 @@ export async function middleware(request: NextRequest) {
     !path.startsWith("/api/") && // Check if it's not an API route
     !token
   ) {
-    // Redirect to sign-in page
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
-
-  // Allow the request to proceed if none of the above conditions are met
   return NextResponse.next();
 }
 
@@ -51,5 +45,6 @@ export const config = {
     "/sign-in",
     "/sign-up",
     "/newshipment/compare",
+    "/contacts",
   ],
 };
